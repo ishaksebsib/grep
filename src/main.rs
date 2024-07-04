baec30aad125e12395d212f10ff7c152d22057fa
ishak <ishaksebsib@gmail.com>
2024-07-04 15:30:32 +0300

    feat: Add basic func to read file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let file_name = &args[1];
+
+    let mut f = File::open(file_name).expect("File not Found ;(");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("Something went wrong reading this file ;(");
+
+    println!("\n--- File Content ---\n\n{}",contents);
+
+
 }
