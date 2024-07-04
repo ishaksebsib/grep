b5da09a6824103c81136039f1c7759d8cce29e1c
ishak <ishaksebsib@gmail.com>
2024-07-04 16:24:58 +0300

    ENH: extracted out the config funcitn from main

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,27 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let file_name = &args[1];
+    let config = parse_config(&args);
 
-    let mut f = File::open(file_name).expect("File not Found ;(");
+    let mut f = File::open(config.file_name).expect("File not Found ;(");
     let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("Something went wrong reading this file ;(");
+    f.read_to_string(&mut contents)
+        .expect("Something went wrong reading this file ;(");
 
-    println!("\n--- File Content ---\n\n{}",contents);
+    println!(
+        "\n---Searching {} in File Content ---\n\n{}",
+        config.query, contents
+    );
+}
 
+struct Config<'a> {
+    query: &'a str,
+    file_name: &'a str,
+}
 
+fn parse_config(args: &Vec<String>) -> Config {
+    let query = &args[1];
+    let file_name = &args[2];
+
+    Config { query, file_name }
 }
