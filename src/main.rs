f8db6e5061e67896094a0437cbf99926b69870d1
ishak <ishaksebsib@gmail.com>
2024-07-04 16:58:50 +0300

    feat: Add erorr handling to config method

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parshing arguments : {}", err);
+        process::exit(1)
+    });
 
     let mut f = File::open(config.file_name).expect("File not Found ;(");
     let mut contents = String::new();
@@ -23,10 +27,14 @@ struct Config<'a> {
 }
 
 impl<'a> Config<'a> {
-    fn new(args: &Vec<String>) -> Config {
+    fn new(args: &'a Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments ;(");
+        }
+
         let query = &args[1];
         let file_name = &args[2];
 
-        Config { query, file_name }
+        Ok(Config { query, file_name })
     }
 }
