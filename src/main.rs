d584c41e46ef7cc5c2eff056c23d002dc8048870
ishak <ishaksebsib@gmail.com>
2024-07-05 10:39:14 +0300

    ENH: spearate out main logint into run function with better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
@@ -10,15 +11,10 @@ fn main() {
         process::exit(1)
     });
 
-    let mut f = File::open(config.file_name).expect("File not Found ;(");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("Something went wrong reading this file ;(");
-
-    println!(
-        "\n---Searching {} in File Content ---\n\n{}",
-        config.query, contents
-    );
+    if let Err(e) = run(config){
+        println!("Application Error ;( {}",e);
+        process::exit(1)
+    }
 }
 
 struct Config<'a> {
@@ -37,4 +33,17 @@ impl<'a> Config<'a> {
 
         Ok(Config { query, file_name })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.file_name)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!(
+        "\n---Searching {} in File Content ---\n\n{}",
+        config.query, contents
+    );
+
+    Ok(())
 }
